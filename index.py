2884957d72299d145fae83fbc46d2d0dde96f898
John Hurliman <jhurliman@jhurliman.org>
2023-01-06 15:30:01 -0800

    Add essentia dependency

--- index.py
+++ index.py
@@ -1,12 +1,24 @@
 import json
 import datetime
 
+from essentia import Pool
+from essentia.standard import (
+    FrameGenerator,
+    HPCP,
+    Key,
+    SpectralPeaks,
+    Spectrum,
+    Windowing,
+)
+
 
 def handler(event, context):
     data = {
-        'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
+        "output": "Hello World",
+        "timestamp": datetime.datetime.utcnow().isoformat(),
     }
-    return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
+    return {
+        "statusCode": 200,
+        "body": json.dumps(data),
+        "headers": {"Content-Type": "application/json"},
+    }
