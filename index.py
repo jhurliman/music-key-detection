344d0c4be76674dad356e7b613d54991e9a9f8d3
John Hurliman <jhurliman@jhurliman.org>
2023-01-06 16:43:25 -0800

    Key detection, unit test

--- index.py
+++ index.py
@@ -1,22 +1,82 @@
 import json
-import datetime
+import numpy as np
 
+from base64 import b64decode
+from datetime import datetime
+from os import remove
 from essentia import Pool
 from essentia.standard import (
     FrameGenerator,
     HPCP,
     Key,
+    MonoLoader,
     SpectralPeaks,
     Spectrum,
     Windowing,
 )
 
+FRAME_SIZE = 2048
+HOP_SIZE = 1024
+
+
+def detect_key(audio):
+    spec = Spectrum(size=FRAME_SIZE)
+    spec_peaks = SpectralPeaks()
+    hpcp = HPCP()
+    key = Key(profileType="edma")
+    w = Windowing(type="blackmanharris92")
+    pool = Pool()
+
+    for frame in FrameGenerator(audio, frameSize=FRAME_SIZE, hopSize=HOP_SIZE):
+        frame_spectrum = spec(w(frame))
+        frequencies, magnitudes = spec_peaks(frame_spectrum)
+        hpcpValue = hpcp(frequencies, magnitudes)
+        pool.add("hpcp", hpcpValue)
+
+    hpcp_avg = np.average(pool["hpcp"], axis=0)
+    detected_key, scale, _, _ = key(hpcp_avg)
+    return detected_key, scale
+
 
 def handler(event, context):
+    # Sanity check "filename" and "content" appear in the event
+    if "filename" not in event or "content" not in event:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": "Missing filename or content"}),
+            "headers": {"Content-Type": "application/json"},
+        }
+
+    # Get the filename and file extension from the event
+    filename = event["filename"]
+    extension = filename.split(".")[-1]
+
+    # Base64 decode the file
+    file_content = b64decode(event["content"])
+
+    # Write the file to a temporary location
+    tmp_filename = "input." + extension
+    with open(tmp_filename, "wb") as f:
+        f.write(file_content)
+
+    # Load the audio file, resampled to a 44.1kHz mono signal
+    loader = MonoLoader(filename=tmp_filename, sampleRate=44100)
+    audio = loader()
+
+    # Detect the key and scale
+    key, scale = detect_key(audio)
+
     data = {
-        "output": "Hello World",
-        "timestamp": datetime.datetime.utcnow().isoformat(),
+        "key": key,
+        "scale": scale,
+        "filename": filename,
+        "filesize": len(file_content),
+        "timestamp": datetime.utcnow().isoformat(),
     }
+
+    # delete the temporary file
+    remove(tmp_filename)
+
     return {
         "statusCode": 200,
         "body": json.dumps(data),
--- tests/test_handler.py
+++ tests/test_handler.py
@@ -3,15 +3,36 @@ import index
 
 
 class TestHandlerCase(unittest.TestCase):
-
-    def test_response(self):
-        print("testing response.")
+    def test_empty_request(self):
+        print("testing empty request.")
         result = index.handler(None, None)
         print(result)
-        self.assertEqual(result['statusCode'], 200)
-        self.assertEqual(result['headers']['Content-Type'], 'application/json')
-        self.assertIn('Hello World', result['body'])
+        self.assertEqual(result["statusCode"], 400)
+        self.assertEqual(result["headers"]["Content-Type"], "application/json")
+        self.assertIn("error", result["body"])
+
+    def test_missing_filename(self):
+        print("testing missing filename.")
+        result = index.handler({"content": "foo"}, None)
+        print(result)
+        self.assertEqual(result["statusCode"], 400)
+        self.assertEqual(result["headers"]["Content-Type"], "application/json")
+        self.assertIn("error", result["body"])
+
+    def test_key_detection(self):
+        filename = "preludeincmajor-30s.mp3"
+        print(f"testing key detection for {filename}.")
+        # load the mp3 into a base64 encoded string
+        with open(filename, "rb") as f:
+            content = f.read()
+        base64_content = content.encode("base64")
+        result = index.handler({"filename": filename, "content": base64_content}, None)
+        print(result)
+        self.assertEqual(result["statusCode"], 200)
+        self.assertEqual(result["headers"]["Content-Type"], "application/json")
+        self.assertIn("key", result["body"])
+        self.assertIn("scale", result["body"])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
